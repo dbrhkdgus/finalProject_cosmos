adf6f7f59119cfeb50f6468d3b5f404e4f4f2f04
JPLEE1991 <whitesunny65@naver.com>
2021-12-18 11:37:03 +0900

    이진표 - 로그인 구현 중 12/18 11:36
    로그인 구현 중

--- src/main/java/com/kh/cosmos/member/model/dao/MemberDao.java
+++ src/main/java/com/kh/cosmos/member/model/dao/MemberDao.java
@@ -1,5 +1,9 @@
 package com.kh.cosmos.member.model.dao;
 
+import com.kh.cosmos.member.model.vo.Member;
+
 public interface MemberDao {
+
+	Member selectOneMember(String id);
 
 }
--- src/main/java/com/kh/cosmos/member/model/service/MemberServiceImpl.java
+++ src/main/java/com/kh/cosmos/member/model/service/MemberServiceImpl.java
@@ -4,9 +4,15 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import com.kh.cosmos.member.model.dao.MemberDao;
+import com.kh.cosmos.member.model.vo.Member;
 
 @Service
 public class MemberServiceImpl implements MemberService {
 	@Autowired
 	private MemberDao memberDao;
+
+	@Override
+	public Member selectOneMember(String id) {
+		return memberDao.selectOneMember(id);
+	}
 }
