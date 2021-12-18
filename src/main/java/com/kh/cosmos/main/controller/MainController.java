debb49b41f53c6733dc9fb7f46c4ae62d4a34440
goofy0205 <goofy0205@gmail.com>
2021-12-18 11:02:05 +0900

    ojdbc의존 추가

--- src/main/java/com/kh/cosmos/main/controller/MainController.java
+++ src/main/java/com/kh/cosmos/main/controller/MainController.java
@@ -1,14 +1,20 @@
 package com.kh.cosmos.main.controller;
 
+import java.util.List;
+
 import javax.servlet.ServletContext;
+import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.core.io.ResourceLoader;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import com.kh.cosmos.main.model.service.MainService;
+import com.kh.cosmos.common.HiSpringUtils;
 
 import lombok.extern.slf4j.Slf4j;
 
@@ -26,7 +32,24 @@ public class MainController {
 	ResourceLoader resourceLoader;
 	
 	@GetMapping("/noticeList.do")
-	public String noticeList() {
+	public String noticeList(@RequestParam(defaultValue = "1") int cPage, Model model, HttpServletRequest request) {
+		log.debug("cPage = {}", cPage);
+		
+		int limit = 10;
+		int offset = (cPage - 1) * limit;
+		
+		List<Notice> list = mainService.selectNoticeList(offset, limit);
+		log.debug("list = {}", list);
+		model.addAttribute("list", list);
+		
+		int totalContent = mainService.selectNoticeTotalCount();
+		log.debug("totalContent = {}", totalContent);
+		model.addAttribute("totalContent", totalContent);
+		
+		String url = request.getRequestURI();
+		String pagebar = HiSpringUtils.getPagebar(cPage, limit, totalContent, url);
+		log.debug("pagebar = {}", pagebar);
+		model.addAttribute("pagebar", pagebar);
 		
 		return "main/notice";
 	}
