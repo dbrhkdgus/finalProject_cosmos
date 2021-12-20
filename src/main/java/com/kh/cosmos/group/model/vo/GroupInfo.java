845fb7bf33052be4a7e4e7960027232b2f2ad47d
bazzyung <bazzy726@gmail.com>
2021-12-20 17:40:46 +0900

    강구진, 김은희, 백지영, 유지우 - 그룹생성 구현중 20일 17:40

--- src/main/java/com/kh/cosmos/group/model/vo/GroupInfo.java
+++ src/main/java/com/kh/cosmos/group/model/vo/GroupInfo.java
@@ -1,5 +1,24 @@
 package com.kh.cosmos.group.model.vo;
 
-public class GroupInfo {
+import java.io.Serializable;
 
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class GroupInfo implements Serializable {
+
+	/**
+	 * 
+	 */
+	private static final long serialVersionUID = 1L;
+	
+	private int giNo;
+	private int seqNo;
+	private String giSubTitle;
+	private String giContent;
+	
 }
