926da536e1e1bd94dfe1fefee5fdc2fc5db9f1d6
bazzyung <bazzy726@gmail.com>
2021-12-23 15:54:26 +0900

    백지영 - 그룹심사 구현중 23일 15:54

--- src/main/java/com/kh/cosmos/group/model/vo/Group.java
+++ src/main/java/com/kh/cosmos/group/model/vo/Group.java
@@ -3,6 +3,8 @@ package com.kh.cosmos.group.model.vo;
 import java.io.Serializable;
 import java.util.Date;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+
 import lombok.AllArgsConstructor;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
@@ -24,6 +26,7 @@ public class Group implements Serializable {
 	private int groupNo;
 	private int categoryNo;
 	private String groupName;
+	@JsonFormat(shape=JsonFormat.Shape.STRING, pattern="yyyy-MM-dd")
 	private Date groupEnrollDate;
 	private char groupCharge;
 	private char groupClose;
