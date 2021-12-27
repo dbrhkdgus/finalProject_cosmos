ee5f5057c2a0c85b5340903a117b9e52f310178b
silver-hee <idsilverni@naver.com>
2021-12-27 21:57:06 +0900

    김은희 -패키지수정 21:56

--- src/main/java/com/kh/cosmos/groupware/fileBoard/dao/FileBoardDao.java
+++
@@ -1,5 +0,0 @@
-package com.kh.cosmos.groupware.fileBoard.dao;
-
-public interface FileBoardDao {
-
-}
--- src/main/java/com/kh/cosmos/groupware/fileBoard/model/dao/FileBoardDaoImpl.java
+++ src/main/java/com/kh/cosmos/groupware/fileBoard/model/dao/FileBoardDaoImpl.java
@@ -1,4 +1,4 @@
-package com.kh.cosmos.groupware.fileBoard.dao;
+package com.kh.cosmos.groupware.fileBoard.model.dao;
 
 import org.springframework.stereotype.Repository;
 
