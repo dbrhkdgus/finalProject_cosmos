41e212bb065f93a8acbbfe6a5ca2b77b41558df8
silver-hee <idsilverni@naver.com>
2021-12-27 17:35:34 +0900

    김은희, 이윤희 - 파일게시판 구현중 17:35

---
+++ src/main/java/com/kh/cosmos/groupware/fileBoard/dao/FileBoardDaoImpl.java
@@ -0,0 +1,8 @@
+package com.kh.cosmos.groupware.fileBoard.dao;
+
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class FileBoardDaoImpl implements FileBoardDao {
+
+}
---
+++ src/main/java/com/kh/cosmos/groupware/fileBoard/service/FileBoardServiceImpl.java
@@ -0,0 +1,8 @@
+package com.kh.cosmos.groupware.fileBoard.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class FileBoardServiceImpl implements FileBoardService {
+
+}
