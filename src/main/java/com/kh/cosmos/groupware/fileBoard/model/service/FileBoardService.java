585d6d2a751acce4673a19434f1e3ee752c3237c
King-Yunhee <kky710160@gmail.com>
2021-12-28 11:49:52 +0900

    이윤희 김은희 - 파일게시판 구현중 12/28 11:49

--- src/main/java/com/kh/cosmos/groupware/fileBoard/model/service/FileBoardService.java
+++ src/main/java/com/kh/cosmos/groupware/fileBoard/model/service/FileBoardService.java
@@ -1,5 +1,9 @@
 package com.kh.cosmos.groupware.fileBoard.model.service;
 
+import com.kh.cosmos.common.attachment.model.vo.Attachment;
+
 public interface FileBoardService {
+
+	int insertFileAttach(Attachment attach);
 
 }
