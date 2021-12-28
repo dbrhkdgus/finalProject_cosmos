e9790b071b10092d7e00d0ccb5a5d9e4e051b124
silver-hee <idsilverni@naver.com>
2021-12-28 23:17:52 +0900

    김은희,이윤희, 강구진(도움 o ) - 파일게시판 기능구현중 (다운로드 구현해야 함) 23:17

--- src/main/java/com/kh/cosmos/groupware/fileBoard/model/dao/FileBoardDao.java
+++ src/main/java/com/kh/cosmos/groupware/fileBoard/model/dao/FileBoardDao.java
@@ -1,7 +1,10 @@
 package com.kh.cosmos.groupware.fileBoard.model.dao;
 
+import java.util.List;
+
 import com.kh.cosmos.common.attachment.model.vo.Attachment;
 import com.kh.cosmos.groupware.board.model.vo.Post;
+import com.kh.cosmos.groupware.board.model.vo.PostWithCategory;
 import com.kh.cosmos.groupware.fileBoard.vo.FileEnroll;
 
 public interface FileBoardDao {
@@ -9,6 +12,8 @@ public interface FileBoardDao {
 	int insertFileAttach(Attachment attach);
 
 	int insertFilePost(Post post);
+
+	List<PostWithCategory> selectAllPostInfileBoard(int boardNo);
 
 	
 
