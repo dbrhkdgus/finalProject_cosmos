d361325992b9270ee261f3f6ca10f7200d5ecec1
goofy0205 <goofy0205@gmail.com>
2022-01-01 23:24:36 +0900

    강구진 - ToDoList 기능구현 완료(정렬구현해야함) 23:24

--- src/main/java/com/kh/cosmos/groupware/app/controller/AppController.java
+++ src/main/java/com/kh/cosmos/groupware/app/controller/AppController.java
@@ -1,7 +1,9 @@
 package com.kh.cosmos.groupware.app.controller;
 
 import java.util.ArrayList;
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 import javax.servlet.ServletContext;
 
@@ -11,10 +13,15 @@ import org.springframework.security.core.Authentication;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 import com.kh.cosmos.common.attachment.model.vo.Attachment;
 import com.kh.cosmos.group.model.vo.Group;
+import com.kh.cosmos.groupware.app.model.service.AppService;
+import com.kh.cosmos.groupware.app.model.vo.TDL;
 import com.kh.cosmos.groupware.board.model.vo.Board;
 import com.kh.cosmos.groupware.chat.model.vo.ChatRoom;
 import com.kh.cosmos.groupware.service.GroupwareService;
@@ -36,11 +43,55 @@ public class AppController {
 	@Autowired
 	private GroupwareService gwService;
 	
+	@Autowired
+	private AppService appService;
+	
 	@GetMapping("/ladder.do")
 	public String ladder(int groupNo, Model model, Authentication auth) {
 		groupwareHeaderSet(groupNo, model, auth);
 		return "gw/app/ladder";
 	}
+	@GetMapping("/TDL.do")
+	public String toDoList(int groupNo, Model model, Authentication auth) {
+		groupwareHeaderSet(groupNo, model, auth);
+		Map<String, Object> param = new HashMap<String, Object>();
+		Member loginMember = (Member) auth.getPrincipal();
+		param.put("groupNo", groupNo);
+		param.put("id", loginMember.getId());
+		List<TDL> tdlList = appService.selectAlltdlListByParam(param);
+		model.addAttribute("tdlList",tdlList);
+		model.addAttribute("loginMember",loginMember);
+		return "gw/app/TDL";
+	}
+	
+	@PostMapping("/insertTDL.do")
+	public String insertTDL(TDL tdl, int groupNo, Model model, Authentication auth, RedirectAttributes redirectAttr) {
+		groupwareHeaderSet(groupNo, model, auth);
+		log.debug("tdl = {}",tdl);
+		Member loginMember = (Member) auth.getPrincipal();
+		
+		int result = appService.insertTdlByGroup(tdl);
+		
+		redirectAttr.addFlashAttribute("msg", result > 0 ? "등록되었습니다." : "등록실패");
+		return "redirect:/gw/app/TDL.do?groupNo="+groupNo;
+	}
+	
+	@ResponseBody
+	@GetMapping("/updateTDL.do")
+	public void updateTDL(int tdlNo, int groupNo, Model model, String check, Authentication auth, RedirectAttributes redirectAttr) {
+		groupwareHeaderSet(groupNo, model, auth);
+		Map<String, Object> param = new HashMap<String, Object>();
+		log.debug("check = {}",check);
+		param.put("check", check);
+		log.debug("tdlNo = {}",tdlNo);
+		param.put("tdlNo", tdlNo);
+		log.debug("groupNo = {}",groupNo);
+		Member loginMember = (Member) auth.getPrincipal();
+		
+		int result = appService.updateTDL(param);
+		
+	}
+	
 	
 	public void groupwareHeaderSet(int groupNo, Model model, Authentication auth) {
 		Member loginMember = (Member) auth.getPrincipal();
---
+++ src/main/java/com/kh/cosmos/groupware/app/model/service/AppService.java
@@ -0,0 +1,16 @@
+package com.kh.cosmos.groupware.app.model.service;
+
+import java.util.List;
+import java.util.Map;
+
+import com.kh.cosmos.groupware.app.model.vo.TDL;
+
+public interface AppService {
+
+	List<TDL> selectAlltdlListByParam(Map<String, Object> param);
+
+	int insertTdlByGroup(TDL tdl);
+
+	int updateTDL(Map<String, Object> param);
+
+}
